9ab3749c431ec82bc45b6313c0d75888f778d9da
mahfuz <mahfuzten@gmail.com>
2025-07-05 20:54:19 +0600

    Initialize variable

---
+++ main.js
@@ -0,0 +1,5 @@
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const btnChoice = document.querySelectorAll("choce-btn");
+const resetBtn = document.getElementById("reset");
+const playerChoice = button.getAttribute("data-choice");
